d8a8d258e8b9fa6cea37ae60519322a9428bd900
Danack <Danack@basereality.com>
2016-03-02 23:03:45 +0000

    Separate example.

--- channel.c
+++ channel.c
@@ -13,9 +13,6 @@ void ThrowWandException(MagickWand *wand) {
     exit(-1); 
 }
 
-  
-//convert Biter_500.jpg -colorspace HSL -separate separate_HSL_%d.gif
-
 int main(int argc,char **argv) {
     
     MagickBooleanType status;
@@ -39,11 +36,11 @@ int main(int argc,char **argv) {
 
     status = MagickSeparateImageChannel(magick_wand, RedChannel);
     
-        status = MagickWriteImages(magick_wand, "channel1.rgb_before_%d.jpg", MagickTrue);
-        if (status == MagickFalse) {
-            ThrowWandException(magick_wand);
-        }
-    
+    status = MagickWriteImages(magick_wand, "channel1.rgb_before_%d.jpg", MagickTrue);
+    if (status == MagickFalse) {
+        ThrowWandException(magick_wand);
+    }
+
 
     //status = MagickSetImageColorspace(hsl_wand, HSLColorspace);
     status = MagickTransformImageColorspace(hsl_wand, HSLColorspace);
